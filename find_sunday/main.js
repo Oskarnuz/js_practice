1f6fb04e22a4153fee2a8a6215f1703b240354bb
Oscar Nuñez <98293549+Oskarnuz@users.noreply.github.com>
2022-05-15 10:32:52 -0500

    Create program to find sunday

---
+++ find_sunday/main.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050. 
+
+const sunday =()=>{
+    for (let i = 2014; i <= 2050; i++) {
+        let date = new Date(i, 0, 1);
+        if (date.getDay() === 0) {
+           alert(`1st January ${i} is a Sunday`);
+        }
+     }    
+}
+
+sunday();
